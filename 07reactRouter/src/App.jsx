76c707db1a03ab9952cbb7b4e29fbf6d66a864f0
jagdish-git <jagadishbal080@gmail.com>
2023-09-30 16:37:45 +0530

    complete curstom hooks, router and context api

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='p-4 bg-slate-500'>React Router Dom</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 07reactRouter/src/components/Github.jsx
@@ -0,0 +1,33 @@
+import React, { useState,useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //   fetch('https://api.github.com/users/hiteshchoudhary')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log(data)
+    //     setData(data)
+    //   })
+    // }, [])
+    
+  return (
+
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+        Github Followers : {data.followers}
+        <p>Name : <i className='text-fuchsia-300'>{data.name}</i></p>
+        <img src={data.avatar_url} alt="Git Profile Picture" width={200}/>
+        
+    </div>
+
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+}
